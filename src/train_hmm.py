b85e5a8696dac11b3250948aa4fd71b6a2d32c90
dmitrii <bakaevdima4@gmail.com>
2026-03-13 12:19:47 +0300

    added a function to train hmm model

---
+++ src/train_hmm.py
@@ -0,0 +1,6 @@
+from hmmlearn import hmm
+
+def train_hmm(X, n_states, n_iter):
+    model = hmm.GaussianHMM(n_components=n_states, covariance_type="full", n_iter=n_iter)
+    model.fit(X)
+    return model
