72954eced90c065c4eaa2c402d25f3dca300cef6
dmitrii <bakaevdima4@gmail.com>
2026-03-13 11:54:02 +0300

    added a function to compute log returns

--- src/get_features.py
+++ src/get_features.py
@@ -1,9 +1,14 @@
 import pandas as pd
+import os
+import numpy as np
 
+DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "data")
 
+def compute_log_returns(df):
+    returns = df['close'] / df['close'].shift(1).dropna()
+    log_returns = np.log(returns)
 
-
-
+    return log_returns
 
 def main():
     pass
