aaeca9adffeffbed3ecedf71ea64218646ec4d74
dmitrii <bakaevdima4@gmail.com>
2026-03-13 12:05:51 +0300

    added a function to compute valoatility features

--- src/get_features.py
+++ src/get_features.py
@@ -9,7 +9,13 @@ def add_log_returns(df, horizons):
         df[f'log_returns_{n}'] = np.log(df['close'] / df['close'].shift(n))
     return df
 
+def add_volatility(df, windows):
+    log_returns = np.log(df['close']).diff()
 
+    for w in windows:
+        df[f'volatility_{w}'] = log_returns.rolling(w).std()
+
+    return df
 
 def main():
     pass
