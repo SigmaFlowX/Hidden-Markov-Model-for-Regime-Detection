573ab839b5f209db09f762271f21e8a309ef6dd3
dmitrii <bakaevdima4@gmail.com>
2026-03-14 17:16:07 +0300

    reworked plot function

--- src/visualization.py
+++ src/visualization.py
@@ -2,9 +2,12 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plot_price_with_regimes(prices, dates, regimes):
-    plt.plot(dates, prices)
-    for i, regime in enumerate(np.unique(regimes)):
-        mask = regimes == regime
-        plt.fill_between(dates, prices.min(), prices.max(), where=mask, alpha=0.2, label=f'Regime {regime}')
-    plt.legend()
-    plt.show()
+    colors = ['red', 'green', 'grey', 'black']
+
+    position = 0
+    for i in range(1, len(regimes)):
+        if regimes[i]!= regimes[i-1]:
+            plt.plot(dates[position:i], prices[position:i], color=colors[regimes[i-1] % len(colors)])
+        position = i
+
+    plt.plot(dates[position:], prices[position:], color=colors[regimes[-1] % len(colors)])
