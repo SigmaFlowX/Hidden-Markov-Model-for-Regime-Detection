674179ef89a06412c6d0a1602adaa64aa37d763c
dmitrii <bakaevdima4@gmail.com>
2026-03-14 17:31:54 +0300

    improved the visuals

--- src/visualization.py
+++ src/visualization.py
@@ -3,6 +3,9 @@ import numpy as np
 
 def plot_price_with_regimes(prices, dates, regimes):
     colors = ['green', 'red', 'grey', 'black']
+    plt.figure(figsize=(8, 4))
+    plt.grid()
+    plt.tight_layout()
 
     start = 0
     for i in range(1, len(regimes)):
