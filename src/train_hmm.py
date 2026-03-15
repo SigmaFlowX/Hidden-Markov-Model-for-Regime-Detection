7ced10a42fee1e8131da832dd20700c2cf9621ce
dmitrii <bakaevdima4@gmail.com>
2026-03-15 12:18:06 +0300

    added verbose as a parameter

--- src/train_hmm.py
+++ src/train_hmm.py
@@ -1,6 +1,6 @@
 from hmmlearn import hmm
 
-def train_hmm(X, n_states, n_iter):
-    model = hmm.GaussianHMM(n_components=n_states, covariance_type="full", n_iter=n_iter)
+def train_hmm(X, n_states, n_iter, verbose=True):
+    model = hmm.GaussianHMM(n_components=n_states, covariance_type="full", n_iter=n_iter, verbose=verbose)
     model.fit(X)
     return model
