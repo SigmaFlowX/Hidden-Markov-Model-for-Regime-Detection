171eb46b6cf3c00016da8a185c66447c757c49ba
dmitrii <bakaevdima4@gmail.com>
2026-03-15 12:38:20 +0300

    added more colors

--- src/visualization.py
+++ src/visualization.py
@@ -2,7 +2,29 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def plot_price_with_regimes(prices, dates, regimes):
-    colors = ['green', 'red', 'gray', 'black']
+    colors = [
+        'green',
+        'red',
+        '#2ca02c',
+        '#d62728',
+        '#9467bd',
+        '#8c564b',
+        '#e377c2',
+        '#7f7f7f',
+        '#bcbd22',
+        '#17becf',
+        '#393b79',
+        '#637939',
+        '#8c6d31',
+        '#843c39',
+        '#7b4173',
+        '#3182bd',
+        '#31a354',
+        '#756bb1',
+        '#636363',
+        '#e6550d'
+    ]
+
     plt.figure(figsize=(8, 4))
     plt.grid()
     plt.tight_layout()
