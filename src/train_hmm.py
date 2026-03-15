e7873017a23dc4a1a3980c72a7b4b2cabb6d43de
dmitrii <bakaevdima4@gmail.com>
2026-03-15 12:30:46 +0300

    added tol as a parameter

--- src/train_hmm.py
+++ src/train_hmm.py
@@ -1,6 +1,6 @@
 from hmmlearn import hmm
 
-def train_hmm(X, n_states, n_iter):
-    model = hmm.GaussianHMM(n_components=n_states, covariance_type="full", n_iter=n_iter)
+def train_hmm(X, n_states, n_iter, tol):
+    model = hmm.GaussianHMM(n_components=n_states, covariance_type="full", n_iter=n_iter, tol=tol)
     model.fit(X)
     return model
