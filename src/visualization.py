30d9942c76d1df2c99af5cc373be606f9c04c593
dmitrii <bakaevdima4@gmail.com>
2026-03-16 10:39:41 +0300

    removed labels since they didnt work well

--- src/visualization.py
+++ src/visualization.py
@@ -33,10 +33,8 @@ def plot_price_with_regimes(prices, dates, regimes):
     start = 0
     for i in range(1, len(regimes)):
         if regimes[i] != regimes[i-1]:
-            plt.plot(dates[start:i], prices[start:i], color=colors[regimes[i-1]], label=regimes[i-1])
+            plt.plot(dates[start:i], prices[start:i], color=colors[regimes[i-1]])
             start = i-1
 
     plt.plot(dates[start:], prices[start:], color=colors[regimes[-1]])
-
-    plt.legend()
     plt.show()
