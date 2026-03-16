c9256ead033eb2bce05c9d54ab81a1cda6aeff8b
dmitrii <bakaevdima4@gmail.com>
2026-03-16 10:51:54 +0300

    added proper labeling

--- src/visualization.py
+++ src/visualization.py
@@ -31,10 +31,16 @@ def plot_price_with_regimes(prices, dates, regimes):
     plt.title("IMOEX over time with predicted regimes")
 
     start = 0
+    used_labels = set()
     for i in range(1, len(regimes)):
         if regimes[i] != regimes[i-1]:
-            plt.plot(dates[start:i], prices[start:i], color=colors[regimes[i-1]])
+
+            label = f"Regime {regimes[i-1]}" if regimes[i-1] not in used_labels else None
+            used_labels.add(regimes[i-1])
+
+            plt.plot(dates[start:i], prices[start:i], color=colors[regimes[i-1]], label=label)
             start = i-1
 
     plt.plot(dates[start:], prices[start:], color=colors[regimes[-1]])
+    plt.legend()
     plt.show()
