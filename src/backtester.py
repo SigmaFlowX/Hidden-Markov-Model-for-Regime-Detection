a5305865ddf89b8c22e46cecb8014c8aa3785f39
Dmitrii Bakaev <bakaevdima4@gmail.com>
2026-06-28 17:18:03 +0800

    loaded data  and added z_score

--- src/backtester.py
+++ src/backtester.py
@@ -2,8 +2,22 @@ import pandas as pd
 import numpy as np
 
 
+def backtest(df, z_entry, z_exit, z_window):
+    mean = df['close'].rolling(window=z_window).mean()
+    std = df['close'].rolling(window=z_window).mean()
+
+    df['z_score'] = (df['close'] - mean)/std
+    df.dropna(inplace=True)
+
+    print(df.head())
+
 def main():
-    pass
+    df = pd.read_csv("../data/SBER.csv")
+    df['timestamp'] = pd.to_datetime(df['begin'])
+    df.set_index('timestamp', inplace=True)
+    df = df[['close']]
+
+    backtest(df, 2, 0.5, 20)
 
 if __name__ == "__main__":
     main()
